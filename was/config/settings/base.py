f7fd111786b6093913d510d16039dab0b507a82a
hdh <hdh@f1security.co.kr>
2022-08-25 10:23:05 +0900

    fix: db 가유

--- was/config/settings/base.py
+++ was/config/settings/base.py
@@ -68,23 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-# database를 사용하기 위한 설정, default로 sqllite를 사용한다.
-DATABASES = {
-    'default': {
-        # TODO : 나중에 배포 및 개발 설정 파일 분리 해야 함
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'cplate',
-        'HOST': 'localhost',
-        'PORT': 5432,
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- was/config/settings/dev.py
+++ was/config/settings/dev.py
@@ -3,7 +3,22 @@ import os
 from unittest.mock import DEFAULT
 from .base import *
 
-
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# database를 사용하기 위한 설정, default로 sqllite를 사용한다.
+DATABASES = {
+    'default': {
+        # TODO : 나중에 배포 및 개발 설정 파일 분리 해야 함
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cplate',
+        'USER': 'cplate',
+        'PASSWORD': 'cplate_password!0C',
+        'HOST': 'donghwa.dev',
+        'PORT': 65200,
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 INSTALLED_APPS += [
     'user'
